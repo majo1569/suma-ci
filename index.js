bd8c0a9ee1d83913067caa9916ce4c309ee98423
majo1569 <ojedamariaj1569@gmail.com>
2021-06-29 12:13:26 -0300

    add docker

--- index.js
+++ index.js
@@ -7,3 +7,20 @@ var b = 5;
 
 
 console.log(`Division ${a} + ${b} = ${calculator.divide(a,b)}`);
+const suma = calculator.divide(a,b); 
+console.log(suma)
+
+const express = require('express')
+
+const app = express()
+
+const port = process.env.PORT || 8000
+
+app.get('/', (req,res) => res.send('hello world'))
+
+async function main(){
+   await app.listen(port);
+   console.log('server is running')
+    }
+main();
+//app.listen(port, () => console.log('Server on port:', port))
